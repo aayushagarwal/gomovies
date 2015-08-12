ce208ccc466d90b70adac66eceb6f1e86b224698
Abhishek Kumar <abhishek.kr.chy@gmail.com>
2015-08-12 23:40:56 +0530

    Create Movie.java

---
+++ src/com/snap/entity/Movie.java
@@ -0,0 +1,49 @@
+package com.snap.entity;
+
+import java.util.Date;
+
+public class Movie {
+	private int movieId;
+	private String movieTitle;
+	private Date releaseDate;
+	private double rating;
+	private double duration;
+	private String description;
+	public int getMovieId() {
+		return movieId;
+	}
+	public void setMovieId(int movieId) {
+		this.movieId = movieId;
+	}
+	public String getMovieTitle() {
+		return movieTitle;
+	}
+	public void setMovieTitle(String movieTitle) {
+		this.movieTitle = movieTitle;
+	}
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	public double getRating() {
+		return rating;
+	}
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+	public double getDuration() {
+		return duration;
+	}
+	public void setDuration(double duration) {
+		this.duration = duration;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
